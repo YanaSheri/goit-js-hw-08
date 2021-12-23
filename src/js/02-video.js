bba6f2273b5dfc75cd5d8278aef5ef15a6886531
YanaSheri <yana.shereheda@gmail.com>
2021-12-23 23:56:03 +0200

    Update 2nd task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from "@vimeo/player";
+import LodashThrottle from "lodash.throttle";
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const onPlay = function (data) {
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(data.seconds));
+};
+
+player.on('timeupdate', LodashThrottle(onPlay, 1000));
+
+const data = localStorage.getItem("videoplayer-current-time");
+player.setCurrentTime(data);

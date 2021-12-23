7cb541a05f9ed204d0248a8dcb6958ae1d8a3d56
YanaSheri <yana.shereheda@gmail.com>
2021-12-23 23:03:04 +0200

    Update 1st test

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
 // Add imports above this line
+import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from "simplelightbox";
 import { galleryItems } from './gallery-items';
+
+
 // Change code below this line
+const listGallery = document.querySelector('.gallery');
+
+const createItems = galleryItems
+    .map(({ preview, original, description }) => {
+        return `<div>
+                    <a class="gallery__item" href="${original}">
+                        <img
+                            class="gallery__image"
+                            src="${preview}"
+                            alt="${description}"
+                        />
+                    </a>
+                </div>`
+    }).join("");
+
+listGallery.insertAdjacentHTML("afterbegin", createItems);
+const cb = (event) => {
+    event.preventDefault();
+    if (event.target.nodeName !== "IMG") return false;
+        const sourceImg = event.target.dataset.source;
+        // console.log(sourceImg);
+    };
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', animationSpeed: "250ms" });
+lightbox.close();
+
+listGallery.addEventListener('click', cb);
 
 console.log(galleryItems);

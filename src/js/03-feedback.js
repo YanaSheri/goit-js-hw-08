26f89e389b15c69ff2c878407df7074895834cd0
YanaSheri <yana.shereheda@gmail.com>
2021-12-24 21:58:33 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -7,12 +7,12 @@ const btn = document.querySelector("button");
 const LOCALSTORAGE_KEY = "feedback-form-state";
 const storage = localStorage.getItem(LOCALSTORAGE_KEY);
 
-const saveMessage = throttle((evt) => {
+const saveMessage = (evt) => {
     evt.preventDefault();
     const dataForm = { email: input.value, message: textArea.value };
     localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(dataForm));
-}, 500);
-form.addEventListener("input", saveMessage);
+};
+form.addEventListener("input", LodashThrottle(saveMessage), 500);
 
 function updateOutput() {
     if (storage) {

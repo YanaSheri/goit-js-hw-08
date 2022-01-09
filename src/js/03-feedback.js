1c799fa054e2ce89034480ff4c7145981c0226d5
YanaSheri <yana.shereheda@gmail.com>
2022-01-09 22:35:07 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -43,3 +43,40 @@ form.addEventListener("submit", cleanForm);
 //     localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(dataForm));
 // }
 // form.addEventListener("input", saveMessage);
+
+//--------------------
+
+
+// import throttle from 'lodash.throttle'; 
+
+// const form = document.querySelector(".feedback-form");
+// const input = document.querySelector("input");
+// const textarea = document.querySelector("textarea");
+// const LOCALSTORAGE_KEY = "feedback-form-state";
+// const formData = {};
+
+// form.addEventListener('submit', onFormSubmit)
+// form.addEventListener('input',throttle(OnTextareaInput, 500));
+
+// populateTextarea();
+
+// function onFormSubmit(event) {
+//     event.preventDefault();
+//     event.currentTarget.reset();
+//     localStorage.removeItem(LOCALSTORAGE_KEY);
+// }
+
+
+// function OnTextareaInput(event) {
+//     formData[event.target.name] = event.target.value;
+//     localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+// }
+
+// function populateTextarea() {
+//     const massege = localStorage.getItem(LOCALSTORAGE_KEY);
+//     const parsedSettings = JSON.parse(massege);
+//     if (parsedSettings) {
+//         input.value = parsedSettings.email; 
+//         textarea.value = parsedSettings.message;
+//     }
+// }
